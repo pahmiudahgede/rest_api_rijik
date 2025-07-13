729a88484f94d00645321826775d7a8ae8f18d34
pahmiudahgede <pahmilucu123@gmail.com>
2025-07-14 00:26:08 +0700

    test

--- config/setup_config.go
+++ config/setup_config.go
@@ -1,27 +1,27 @@
 
 package config
 import (
-	"log"
-	"os"
+	// "log"
+	// "os"
 
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 func SetupConfig() {
-	if _, exists := os.LookupEnv("DOCKER_ENV"); exists {
-		log.Println("Running in Docker container, using environment variables")
-	} else {
-		err := godotenv.Load(".env.dev")
-		if err != nil {
-			log.Printf("Warning: Error loading .env file: %v", err)
-			log.Println("Trying to use system environment variables...")
-		} else {
-			log.Println("Loaded environment from .env.dev file")
-		}
-	}
+	// if _, exists := os.LookupEnv("DOCKER_ENV"); exists {
+	// 	log.Println("Running in Docker container, using environment variables")
+	// } else {
+	// 	err := godotenv.Load(".env.dev")
+	// 	if err != nil {
+	// 		log.Printf("Warning: Error loading .env file: %v", err)
+	// 		log.Println("Trying to use system environment variables...")
+	// 	} else {
+	// 		log.Println("Loaded environment from .env.dev file")
+	// 	}
+	// }
 	ConnectDatabase()
 	ConnectRedis()
 	go func() {
-		InitWhatsApp() // Ini tidak akan blocking startup server
+		InitWhatsApp() 
 	}()
 }
